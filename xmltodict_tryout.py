f20428613a068ea7d49b2c14b1c028760a39acf9
piyushghate <piyushghate17@gmail.com>
2018-11-16 15:05:46 +0530

    Multiple Authors working!

--- xmltodict_tryout.py
+++ xmltodict_tryout.py
@@ -2,10 +2,16 @@ import urllib.request
 import xmltodict
 
 
-with urllib.request.urlopen("https://www.goodreads.com/book/show/12177850.xml?key=VugSymVo7nXix0hvwXr0w") as url:
+with urllib.request.urlopen("https://www.goodreads.com/book/show/12067.xml?key=VugSymVo7nXix0hvwXr0w") as url:
             data = url.read()
 
 
 data = xmltodict.parse(data)
 # print(render_to_response('my_template.html', {'data': data}))
 print(data['GoodreadsResponse']['book']['id'])
+
+list1 = []
+for author in data['GoodreadsResponse']['book']['authors']['author']:
+    print(author['name'])
+
+# print(data['GoodreadsResponse']['book']['authors']['author']['name'])
