beae67a22217b7827184678cd749baec83366c41
Piyush Ghate <piyushghate17@gmail.com>
2018-11-19 21:44:29 +0530

    post request to GoodReads API

--- input_to_class.py
+++ input_to_class.py
@@ -1,11 +1,23 @@
-from class_try import GoodReadsAPIClient
+# from class_try import GoodReadsAPIClient
 
 
-# "https://www.goodreads.com/book/show/12177850-a-song-of-ice-and-fire"
-# "https://www.goodreads.com/book/show/12067.Good_Omens"
+# # "https://www.goodreads.com/book/show/12177850-a-song-of-ice-and-fire"
+# # "https://www.goodreads.com/book/show/12067.Good_Omens"
 
-input_from_user = input('Please mention a valid GoodReads url for book info: ') # input from user
+# input_from_user = input('Please mention a valid GoodReads url for book info: ') # input from user
 
-book1 = GoodReadsAPIClient(input_from_user)     # initializing the class with book1 instance
+# book1 = GoodReadsAPIClient(input_from_user)     # initializing the class with book1 instance
 
-print(GoodReadsAPIClient.get_book_details(book1))   # calling the method inside of the class
+# print(book1.get_book_details())   # calling the method inside of the class
+
+
+
+import requests
+import json
+
+# making a post request @http://127.0.0.1:5000/repos with header as ({'Content-Type': 'application/json'}) and data as ({'org': 'google'})
+r = requests.post('http://127.0.0.1:5000/details', data=json.dumps({'book_url': 'https://www.goodreads.com/book/show/12177850-a-song-of-ice-and-fire'}), headers={'Content-Type': 'application/json'})
+
+print(r)            # to check the status code 
+print(r.headers)    # to print the header info
+print(r.content)
